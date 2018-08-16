3c9e3f420c0f38c2b46c83488256904217ad86b2
ZEvans1 <zte.zachary@gmail.com>
2018-08-16 11:12:37 -0700

    Add initial code for other components.

---
+++ src/components/landingpage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  }
+}
+
+export default Landing;
---
+++ src/components/projects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    <div>
+      <h1>Projects Page</h1>
+    </div>
+  }
+}
+
+export default Projects;
