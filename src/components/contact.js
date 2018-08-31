9b3f003c32f4515d2f52fe2c06aa3ac337b3ae40
ZEvans1 <zte.zachary@gmail.com>
2018-08-30 17:25:17 -0700

    Add text and image for left side of grid.

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,8 +6,14 @@ class Contact extends Component {
     return(
       <div className="contact-body">
         <Grid className="contact-grid">
-          <Cell col={6} >Half of page</Cell>
-          <Cell col={6} >Half of page</Cell>
+          <Cell col={6}>
+            <h2>Zach Evans</h2>
+            <img src="" alt="avatar"
+              style={{height: '250px'}}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Here is some filler text that I am using instead of lorem ipsum. I dont like using it all the time so here is this text</p>
+          </Cell>
+          <Cell col={6}>Half of page</Cell>
         </Grid>
       </div>
     )
