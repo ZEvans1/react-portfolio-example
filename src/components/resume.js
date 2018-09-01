4830b401a22b41a38380e71c12b09808e5bd1183
ZEvans1 <zte.zachary@gmail.com>
2018-08-31 17:31:54 -0700

    Add columns in resume section.

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
   render() {
     return(
       <div>
-        <h1>Resume Page</h1>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+                src=""
+                alt="Avatar"
+                style={{height: '200px'}}
+              />
+            </div>
+            <h2 style={{paddingTop: '2em'}}>Zach Evans</h2>
+            <h4 style={{color: 'grey'}}>Programmer</h4>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>Right Side</Cell>
+        </Grid>
       </div>
     )
   }
