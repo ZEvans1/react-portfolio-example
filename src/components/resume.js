8c7beda09b415bbd2577cec4f6a21ef133e9f382
ZEvans1 <zte.zachary@gmail.com>
2018-08-31 18:49:54 -0700

    Add resume section.

--- src/components/resume.js
+++ src/components/resume.js
@@ -16,6 +16,18 @@ class Resume extends Component {
             </div>
             <h2 style={{paddingTop: '2em'}}>Zach Evans</h2>
             <h4 style={{color: 'grey'}}>Programmer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>This is some filler text to go here. Yeah I really hate lorem ipsum that much.</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h5>Address</h5>
+            <p></p>
+            <h5>Phone</h5>
+            <p></p>
+            <h5>Email</h5>
+            <p></p>
+            <h5>Web</h5>
+            <p></p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
           </Cell>
           <Cell className="resume-right-col" col={8}>Right Side</Cell>
         </Grid>
