2bb3bd72711c85285d5ba1a1101d9dc8c072300f
ZEvans1 <zte.zachary@gmail.com>
2018-09-05 11:13:48 -0700

    Add a skills section with progress bars.

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import Education from './education';
 import Experience from './experience';
+import Skills from './skills';
 
 class Resume extends Component {
   render() {
@@ -59,6 +60,11 @@ class Resume extends Component {
               endYear={2017}
               jobName="Mobile Development Intern"
               jobDescription="This is some text about the job description"
+            />
+            <h2>Skills</h2>
+            <Skills
+              skill="JavaScript"
+              progress={70}
             />
           </Cell>
         </Grid>
